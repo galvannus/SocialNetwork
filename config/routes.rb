9d44c22236aa81bce4f66f180c7a6e0f8ddee486
Jorge Alejandro Marcial Galvan <galvannus@gmail.com>
2018-04-30 12:37:16 -0600

    Primer commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'main/home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+  	root to: "main#home"
+  end
+  unauthenticated :user do
+  	root to: "main#unregistered"
+  end
+end
